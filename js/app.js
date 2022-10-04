4bf385531614fa6da66e13e09d6ed9d239265ea3
jiayuweng <qwe372129@gmail.com>
2022-10-04 22:00:31 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/webAR_test_muti.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/AR.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
